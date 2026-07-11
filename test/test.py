2f6d62ae491ad8a28c3b242245a62a1617083114
Kazto Kitabatake <kitabatake1013@outlook.jp>
2026-07-12 08:56:41 +0900

    動作確認用プログラムの仕様変更を完全にコミットできていなかったのを修正

--- test/test.py
+++ test/test.py
@@ -1,10 +1,13 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from englishToKanaConverter import EnglishToKanaConverter
 
 
 if __name__ == "__main__":
-    c = EnglishToKanaConverter(True, os.path.join(os.path.dirname(__file__), "englishToKanaConverter.log"))
+    c = EnglishToKanaConverter(True, os.path.join(os.path.dirname(__file__), "log.txt"))
     while True:
         try:
             text = input("文字列を入力（終了はCtrl+C）:")
